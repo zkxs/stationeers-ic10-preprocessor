137b92943d7339b00246c884e1e73e7e8df68160
Michael Ripley <zkxs00@gmail.com>
2022-08-06 04:26:46 -0500

    Ridiculous `yield; j` optimization

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use regex::Regex;
 use lazy_static::lazy_static;
 use itertools::Itertools;
 
+const YIELD_INSTRUCTION: &str = "yield";
+
 lazy_static! {
     static ref LABEL_REGEX: Regex = Regex::new(r"^(\w+):").unwrap();
+    static ref JUMP_REGEX: Regex = Regex::new(r"j (\w+)").unwrap();
 }
 
 fn main() {
@@ -34,12 +37,25 @@ fn process_file(filename: &str) -> io::Result<()> {
         let line = line?;
 
         if let Some(captures) = LABEL_REGEX.captures(&line) {
-            let label = captures.get(1).unwrap();
-            let label: String = label.as_str().to_owned();
+            let label = captures.get(1).unwrap().as_str();
             labels.push(Label {
-                label,
+                label: label.to_owned(),
                 line: line_number.to_string(),
             });
+        } else if let Some(captures) = JUMP_REGEX.captures(&line) {
+            let label = captures.get(1).unwrap().as_str();
+            let previous_was_yield: bool = program.last()
+                .map(|last| last == YIELD_INSTRUCTION)
+                .unwrap_or(false);
+            if previous_was_yield {
+                // nuke the yield and sneak a - in front of the label
+                program.pop();
+                program.push(format!("j -{}", label));
+            } else {
+                // do nothing special
+                program.push(line);
+                line_number += 1;
+            }
         } else {
             program.push(line);
             line_number += 1;
